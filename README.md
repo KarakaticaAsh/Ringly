0ff97be99b197908d7457e210f3cd70f472361ba
Antony <shulezhko.antony@gmail.com>
2024-10-10 17:11:35 +0000

    theme changed

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 https://koldovsky.github.io/online-shop-htmx/
+
+here goes my first project - website for selling purposes
+my product is the smart ring with nfc payment function 
+website has Lord Of Rings style because i`m reading this book right now :)
--- js/alert.js
+++ js/alert.js
@@ -1,6 +1,7 @@
 export function showAlert(message, success = true) {
-  document.querySelector(".alert-container").innerHTML = `<div class="alert ${success ? "alert-success" : "alert-danger"
-    } alert-dismissible fade show" role="alert">
+  document.querySelector(".alert-container").innerHTML = `<div class="alert ${
+    success ? "alert-success" : "alert-danger"
+  } alert-dismissible fade show" role="alert">
           <strong>${message}</strong>
           <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
      </div>`;
